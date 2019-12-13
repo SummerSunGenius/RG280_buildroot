04ffafcf7976e202f583d6bcb155ef4fa5aa1876
Gleb Mazovetskiy <glex.spb@gmail.com>
2019-12-13 10:18:04 +0000

    libcurl: old buildroot compat

--- package/libcurl/libcurl.mk
+++ package/libcurl/libcurl.mk
@@ -20,20 +20,20 @@ LIBCURL_INSTALL_STAGING = YES
 # http://curl.haxx.se/docs/manpage.html#--ntlm.
 # Likewise, there is no compiler on the target, so libcurl-option (to
 # generate C code) isn't very useful
-LIBCURL_CONF_OPTS = --disable-manual --disable-ntlm-wb \
+LIBCURL_CONF_OPT = --disable-manual --disable-ntlm-wb \
 	--enable-hidden-symbols --with-random=/dev/urandom --disable-curldebug \
 	--without-polarssl --disable-libcurl-option
 
 ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
-LIBCURL_CONF_OPTS += --enable-threaded-resolver
+LIBCURL_CONF_OPT += --enable-threaded-resolver
 else
-LIBCURL_CONF_OPTS += --disable-threaded-resolver
+LIBCURL_CONF_OPT += --disable-threaded-resolver
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCURL_VERBOSE),y)
-LIBCURL_CONF_OPTS += --enable-verbose
+LIBCURL_CONF_OPT += --enable-verbose
 else
-LIBCURL_CONF_OPTS += --disable-verbose
+LIBCURL_CONF_OPT += --disable-verbose
 endif
 
 LIBCURL_CONFIG_SCRIPTS = curl-config
@@ -45,85 +45,85 @@ LIBCURL_DEPENDENCIES += openssl
 # Fix it by setting LD_LIBRARY_PATH to something sensible so those libs
 # are found first.
 LIBCURL_CONF_ENV += LD_LIBRARY_PATH=$(if $(LD_LIBRARY_PATH),$(LD_LIBRARY_PATH):)/lib:/usr/lib
-LIBCURL_CONF_OPTS += --with-ssl=$(STAGING_DIR)/usr \
+LIBCURL_CONF_OPT += --with-ssl=$(STAGING_DIR)/usr \
 	--with-ca-path=/etc/ssl/certs
 else
-LIBCURL_CONF_OPTS += --without-ssl
+LIBCURL_CONF_OPT += --without-ssl
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCURL_GNUTLS),y)
-LIBCURL_CONF_OPTS += --with-gnutls=$(STAGING_DIR)/usr \
+LIBCURL_CONF_OPT += --with-gnutls=$(STAGING_DIR)/usr \
 	--with-ca-fallback
 LIBCURL_DEPENDENCIES += gnutls
 else
-LIBCURL_CONF_OPTS += --without-gnutls
+LIBCURL_CONF_OPT += --without-gnutls
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCURL_LIBNSS),y)
-LIBCURL_CONF_OPTS += --with-nss=$(STAGING_DIR)/usr
+LIBCURL_CONF_OPT += --with-nss=$(STAGING_DIR)/usr
 LIBCURL_CONF_ENV += CPPFLAGS="$(TARGET_CPPFLAGS) `$(PKG_CONFIG_HOST_BINARY) nspr nss --cflags`"
 LIBCURL_DEPENDENCIES += libnss
 else
-LIBCURL_CONF_OPTS += --without-nss
+LIBCURL_CONF_OPT += --without-nss
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCURL_MBEDTLS),y)
-LIBCURL_CONF_OPTS += --with-mbedtls=$(STAGING_DIR)/usr
+LIBCURL_CONF_OPT += --with-mbedtls=$(STAGING_DIR)/usr
 LIBCURL_DEPENDENCIES += mbedtls
 else
-LIBCURL_CONF_OPTS += --without-mbedtls
+LIBCURL_CONF_OPT += --without-mbedtls
 endif
 
 ifeq ($(BR2_PACKAGE_C_ARES),y)
 LIBCURL_DEPENDENCIES += c-ares
-LIBCURL_CONF_OPTS += --enable-ares
+LIBCURL_CONF_OPT += --enable-ares
 else
-LIBCURL_CONF_OPTS += --disable-ares
+LIBCURL_CONF_OPT += --disable-ares
 endif
 
 ifeq ($(BR2_PACKAGE_LIBIDN2),y)
 LIBCURL_DEPENDENCIES += libidn2
-LIBCURL_CONF_OPTS += --with-libidn2
+LIBCURL_CONF_OPT += --with-libidn2
 else
-LIBCURL_CONF_OPTS += --without-libidn2
+LIBCURL_CONF_OPT += --without-libidn2
 endif
 
 # Configure curl to support libssh2
 ifeq ($(BR2_PACKAGE_LIBSSH2),y)
 LIBCURL_DEPENDENCIES += libssh2
-LIBCURL_CONF_OPTS += --with-libssh2
+LIBCURL_CONF_OPT += --with-libssh2
 else
-LIBCURL_CONF_OPTS += --without-libssh2
+LIBCURL_CONF_OPT += --without-libssh2
 endif
 
 ifeq ($(BR2_PACKAGE_BROTLI),y)
 LIBCURL_DEPENDENCIES += brotli
-LIBCURL_CONF_OPTS += --with-brotli
+LIBCURL_CONF_OPT += --with-brotli
 else
-LIBCURL_CONF_OPTS += --without-brotli
+LIBCURL_CONF_OPT += --without-brotli
 endif
 
 ifeq ($(BR2_PACKAGE_NGHTTP2),y)
 LIBCURL_DEPENDENCIES += nghttp2
-LIBCURL_CONF_OPTS += --with-nghttp2
+LIBCURL_CONF_OPT += --with-nghttp2
 else
-LIBCURL_CONF_OPTS += --without-nghttp2
+LIBCURL_CONF_OPT += --without-nghttp2
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCURL_COOKIES_SUPPORT),y)
-LIBCURL_CONF_OPTS += --enable-cookies
+LIBCURL_CONF_OPT += --enable-cookies
 else
-LIBCURL_CONF_OPTS += --disable-cookies
+LIBCURL_CONF_OPT += --disable-cookies
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCURL_PROXY_SUPPORT),y)
-LIBCURL_CONF_OPTS += --enable-proxy
+LIBCURL_CONF_OPT += --enable-proxy
 else
-LIBCURL_CONF_OPTS += --disable-proxy
+LIBCURL_CONF_OPT += --disable-proxy
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCURL_EXTRA_PROTOCOLS_FEATURES),y)
-LIBCURL_CONF_OPTS += \
+LIBCURL_CONF_OPT += \
 	--enable-dict \
 	--enable-gopher \
 	--enable-imap \
@@ -136,7 +136,7 @@ LIBCURL_CONF_OPTS += \
 	--enable-telnet \
 	--enable-tftp
 else
-LIBCURL_CONF_OPTS += \
+LIBCURL_CONF_OPT += \
 	--disable-dict \
 	--disable-gopher \
 	--disable-imap \
@@ -163,7 +163,7 @@ LIBCURL_POST_INSTALL_TARGET_HOOKS += LIBCURL_TARGET_CLEANUP
 endif
 
 HOST_LIBCURL_DEPENDENCIES = host-openssl
-HOST_LIBCURL_CONF_OPTS = \
+HOST_LIBCURL_CONF_OPT = \
 	--disable-manual \
 	--disable-ntlm-wb \
 	--disable-curldebug \
