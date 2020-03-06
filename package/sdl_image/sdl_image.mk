834fb9f8f238aabbe4d5a85dfa16816fab39af34
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-03-06 06:01:07 -0800

    Update SDL_Image to latest hg a66b2dd7687b
    This version of SDL_Image contains a configure.ac fix to pick up the latest versions of the libraries.
    
    This ensures SDL_Image dynamically loads libpng16 and jpeg9 instead of the older versions.

--- package/sdl_image/sdl_image.mk
+++ package/sdl_image/sdl_image.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-SDL_IMAGE_VERSION = 1.2.12
-SDL_IMAGE_SOURCE = SDL_image-$(SDL_IMAGE_VERSION).tar.gz
-SDL_IMAGE_SITE = http://www.libsdl.org/projects/SDL_image/release
+SDL_IMAGE_VERSION = a66b2dd7687b
+SDL_IMAGE_SOURCE = $(SDL_IMAGE_VERSION).tar.gz
+SDL_IMAGE_SITE = https://hg.libsdl.org/SDL_image/archive
 SDL_IMAGE_INSTALL_STAGING = YES
 SDL_IMAGE_LICENSE = zlib
 SDL_IMAGE_LICENSE_FILES = COPYING
