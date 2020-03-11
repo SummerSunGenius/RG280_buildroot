2f1d4ae0ed79b17decb43e0af47aa21fac1d98b3
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-03-11 11:15:45 +0000

    package/pkgconf: old buildroot backwards compat

--- package/pkgconf/pkgconf.mk
+++ package/pkgconf/pkgconf.mk
@@ -10,7 +10,7 @@ PKGCONF_SOURCE = pkgconf-$(PKGCONF_VERSION).tar.xz
 PKGCONF_LICENSE = pkgconf license
 PKGCONF_LICENSE_FILES = COPYING
 
-PKG_CONFIG_HOST_BINARY = $(HOST_DIR)/bin/pkg-config
+PKG_CONFIG_HOST_BINARY = $(HOST_DIR)/usr/bin/pkg-config
 
 define PKGCONF_LINK_PKGCONFIG
 	ln -sf pkgconf $(TARGET_DIR)/usr/bin/pkg-config
@@ -18,23 +18,23 @@ endef
 
 define HOST_PKGCONF_INSTALL_WRAPPER
 	$(INSTALL) -m 0755 -D package/pkgconf/pkg-config.in \
-		$(HOST_DIR)/bin/pkg-config
+		$(HOST_DIR)/usr/bin/pkg-config
 	$(SED) 's,@STAGING_SUBDIR@,$(STAGING_SUBDIR),g' \
-		$(HOST_DIR)/bin/pkg-config
+		$(HOST_DIR)/usr/bin/pkg-config
 endef
 
 define HOST_PKGCONF_STATIC
-	$(SED) 's,@STATIC@,--static,' $(HOST_DIR)/bin/pkg-config
+	$(SED) 's,@STATIC@,--static,' $(HOST_DIR)/usr/bin/pkg-config
 endef
 
 define HOST_PKGCONF_SHARED
-	$(SED) 's,@STATIC@,,' $(HOST_DIR)/bin/pkg-config
+	$(SED) 's,@STATIC@,,' $(HOST_DIR)/usr/bin/pkg-config
 endef
 
 PKGCONF_POST_INSTALL_TARGET_HOOKS += PKGCONF_LINK_PKGCONFIG
 HOST_PKGCONF_POST_INSTALL_HOOKS += HOST_PKGCONF_INSTALL_WRAPPER
 
-ifeq ($(BR2_STATIC_LIBS),y)
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
 HOST_PKGCONF_POST_INSTALL_HOOKS += HOST_PKGCONF_STATIC
 else
 HOST_PKGCONF_POST_INSTALL_HOOKS += HOST_PKGCONF_SHARED
