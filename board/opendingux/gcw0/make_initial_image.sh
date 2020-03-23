9145c92eb77702437dde7dc40d44d2762b72289a
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-03-23 01:06:14 +0000

    make_initial_image improvements
    1. Use built OPKs instead of downloading them.
    2. Don't create a hibernation partition (factory image doesn't have any)

--- board/opendingux/gcw0/make_initial_image.sh
+++ board/opendingux/gcw0/make_initial_image.sh
@@ -19,34 +19,23 @@ board/opendingux/gcw0/build_mininit.sh
 # remove previous packages
 rm -rf output/images/boot.vfat
 rm -rf output/images/apps.ext4
-rm -rf output/images/hibernation.ext4
 rm -rf output/images/sdcard.img
 
-# local_pack_root is where the factory version emulators and ports should reside
-mkdir -p output/images/local_pack_root/apps
-
-echo Downloading rs97.bitgala.xyz/RG-350/localpack/emulators/...
-( cd  output/images/local_pack_root/apps ; \
-wget -N -r -nd --no-parent --reject='index.html*' --reject='scumm*' --reject='robots.txt*' \
-https://rs97.bitgala.xyz/RG-350/localpack/default_emulators/ ) &> /tmp/emulators.log
-
-echo Downloading rs97.bitgala.xyz/RG-350/localpack/apps/...
-( cd  output/images/local_pack_root/apps ; \
-wget -N -r -nd --no-parent --reject='index.html*' --reject='robots.txt*' \
-https://rs97.bitgala.xyz/RG-350/localpack/default_apps/ ) &> /tmp/apps.log
-
-# hiberation_root is just an empty directory to trick genimage into creating
-# an empty ext4 partition.  Always clear it out, as it should be empty
-mkdir -p /tmp/hiberation_root
-rm -rf /tmp/hiberation_root/*
+# Copy OPKs
+mkdir -p output/images/apps_partition/apps/
+cp output/images/opks/*.opk output/images/apps_partition/apps/
 
 # Clear output/images-tmp
 rm -rf output/images-tmp
 
+# Empty rootfs
+rm -rf output/genimage-empty-root
+mkdir output/genimage-empty-root
+
 # Build sdcard.img from board/opendingux/gcw0/genimage.cfg
 output/host/usr/bin/genimage \
 	    --config board/opendingux/gcw0/genimage.cfg \
-	    --rootpath /tmp/hiberation_root \
+	    --rootpath output/genimage-empty-root \
 	    --inputpath output/images \
 	    --outputpath output/images \
 	    --tmppath output/images-tmp
