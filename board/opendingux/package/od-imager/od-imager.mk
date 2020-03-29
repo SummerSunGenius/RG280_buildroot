fb5ba1a64b742bfcf215de55e4f7f234567880ab
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-03-29 22:53:01 +0100

    package/od-image: update version

--- board/opendingux/package/od-imager/od-imager.mk
+++ board/opendingux/package/od-imager/od-imager.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OD_IMAGER_VERSION = a95b368b35e979a1cfefb0fb2b81e5ee3bab9064
+OD_IMAGER_VERSION = ff98a7e13f
 OD_IMAGER_SITE = $(call github,glebm,imager,$(OD_IMAGER_VERSION))
 
 define HOST_OD_IMAGER_INSTALL_CMDS
