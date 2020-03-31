43eaeb8efb24bd4993a6e9a77ca2bd6c7702c557
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-03-31 20:50:40 +0100

    package/oprofile: Update from upstream

--- package/oprofile/oprofile.mk
+++ package/oprofile/oprofile.mk
@@ -4,25 +4,23 @@
 #
 ################################################################################
 
-OPROFILE_VERSION = 0.9.9
+OPROFILE_VERSION = 1.3.0
 OPROFILE_SITE = http://downloads.sourceforge.net/project/oprofile/oprofile/oprofile-$(OPROFILE_VERSION)
-OPROFILE_LICENSE = GPLv2+
+OPROFILE_LICENSE = GPL-2.0+
 OPROFILE_LICENSE_FILES = COPYING
+# 0002-configure.ac-add-foreign-option-to-AM_INIT_AUTOMAKE.patch
+# 0003-Macro-wrappers-to-handle-the-binutils-2.34-api-chang.patch
+OPROFILE_AUTORECONF = YES
 OPROFILE_CONF_OPTS = \
-	--localstatedir=/var \
 	--disable-account-check \
 	--enable-gui=no \
 	--with-kernel=$(STAGING_DIR)/usr
-OPROFILE_AUTORECONF = YES
-OPROFILE_BINARIES = utils/ophelp pp/opannotate pp/oparchive pp/opgprof
-OPROFILE_BINARIES += pp/opreport opjitconv/opjitconv daemon/oprofiled
-OPROFILE_BINARIES += utils/op-check-perfevents libabi/opimport
-OPROFILE_BINARIES += pe_counting/ocount
 
-# No perf_events support in kernel for avr32
-ifneq ($(BR2_avr32),y)
-OPROFILE_BINARIES += pe_profiling/operf
-endif
+OPROFILE_BINARIES = \
+	utils/ophelp pp/opannotate pp/oparchive pp/opgprof \
+	pp/opreport opjitconv/opjitconv \
+	utils/op-check-perfevents libabi/opimport \
+	pe_counting/ocount pe_profiling/operf
 
 ifeq ($(BR2_i386),y)
 OPROFILE_ARCH = i386
@@ -46,11 +44,11 @@ ifeq ($(BR2_PACKAGE_LIBPFM4),y)
 OPROFILE_DEPENDENCIES += libpfm4
 endif
 
-define OPROFILE_CREATE_FILES
-	touch $(@D)/NEWS $(@D)/AUTHORS $(@D)/ChangeLog
+ifeq ($(BR2_STATIC_LIBS),)
+define OPROFILE_INSTALL_SHARED_LIBRARY
+	$(INSTALL) -m 755 $(@D)/libopagent/.libs/*.so* $(TARGET_DIR)/usr/lib/oprofile
 endef
-
-OPROFILE_POST_PATCH_HOOKS += OPROFILE_CREATE_FILES
+endif
 
 define OPROFILE_INSTALL_TARGET_CMDS
 	$(INSTALL) -d -m 755 $(TARGET_DIR)/usr/bin
@@ -61,9 +59,8 @@ define OPROFILE_INSTALL_TARGET_CMDS
 			$(TARGET_DIR)/usr/share/oprofile; \
 	fi
 	$(INSTALL) -m 644 $(@D)/libregex/stl.pat $(TARGET_DIR)/usr/share/oprofile
-	$(INSTALL) -m 755 $(@D)/utils/opcontrol $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 $(addprefix $(@D)/, $(OPROFILE_BINARIES)) $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 $(@D)/libopagent/.libs/*.so* $(TARGET_DIR)/usr/lib/oprofile
+	$(OPROFILE_INSTALL_SHARED_LIBRARY)
 endef
 
 $(eval $(autotools-package))
