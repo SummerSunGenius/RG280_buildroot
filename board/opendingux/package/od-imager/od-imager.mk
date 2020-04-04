0b6f9ae02ddd8b11bb1e157665c17e5214c294f7
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-04-04 07:41:29 +0100

    Update kernel and imager versions

--- board/opendingux/package/od-imager/od-imager.mk
+++ board/opendingux/package/od-imager/od-imager.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OD_IMAGER_VERSION = b60c8374b
+OD_IMAGER_VERSION = 6a44fc40565c
 OD_IMAGER_SITE = $(call github,glebm,imager,$(OD_IMAGER_VERSION))
 
 define HOST_OD_IMAGER_INSTALL_CMDS
